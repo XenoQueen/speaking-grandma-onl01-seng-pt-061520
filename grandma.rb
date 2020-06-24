0a3d156494fb0f281fafd7867d9612277111a3ab
Maika Kailani <queenmab051@gmail.com>
2020-06-24 16:43:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = phrase.downcase
-  return
+    return "HUH"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
